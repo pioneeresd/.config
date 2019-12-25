fd21b59b799a2c70ad51a65ea833d40a4f821943
Jim Hessin <jhessin@gmail.com>
2019-12-24 21:35:14 -0700

    Updated to use xautolock instead of xidlehook

--- install.sh
+++ install.sh
@@ -7,10 +7,10 @@ sudo pacman -Scc --noconfirm
 sudo pacman -Rdd --noconfirm libdmx libxxf86dga
 sudo pacman -Syyu --noconfirm --needed &&
 sudo pacman -Rdd --noconfirm xorgproto
-yay -Syu --noconfirm --needed --noredownload --norebuild
+yay -Syu --noconfirm --needed --noredownload --norebuild &&
 
 # Then install the necessary programs
-get="yay -S --noconfirm --needed --noredownload --norebuild"
+get="yay -S --noconfirm --needed --noredownload --norebuild" &&
 $get libreoffice-fresh &&
 $get neovim &&
 $get python &&
@@ -21,4 +21,4 @@ $HOME/.config/nvim/install.sh &&
 $get rubygems &&
 $get ruby-irb &&
 $get i3lock &&
-$get xidlehook 
+$get xautolock 
